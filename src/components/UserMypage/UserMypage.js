789b8817a47050c9d6f60da9f4c3caa5150a9b75
kitKimBeomSeok <126782965+kitKimBeomSeok@users.noreply.github.com>
2023-06-06 14:56:02 +0900

    nav 경로 누락 추가 (changeform, orderhistory)

--- src/components/UserMypage/UserMypage.js
+++ src/components/UserMypage/UserMypage.js
@@ -3,9 +3,19 @@ import './css/UserMypage.css';
 import Nav from "../Nav/Nav";
 import React from "react";
 import ChangeInform from "../ChangeInform/ChangeInform";
+import {useNavigate} from "react-router-dom";
 
 function UserMypage() {
+    const navigate = useNavigate();
+
+    const handleOrderHistoryClick = () =>{
+        navigate("/OrderHistory")
+    }
+    const handleChangeInform = () =>{
+        navigate("/ChangeInform")
+    }
   return (
+
     <div>
         <Nav/>
 
@@ -16,7 +26,7 @@ function UserMypage() {
       </div>
       <div className='tab'>
         <div className='tabTitle'>
-            <div className='tabtext'>주문내역</div>
+            <div className='tabtext'onClick={()=>handleOrderHistoryClick()}>주문내역</div>
         </div>
         <div className='tabTitle'>
             <div className='tabtext'>쿠폰함</div>
@@ -28,7 +38,7 @@ function UserMypage() {
             <div className='tabtext'>비행기스탬프</div>
         </div>
         <div className='tabTitle'>
-            <div className='tabtext'>정보수정</div>
+            <div className='tabtext'onClick={()=>handleChangeInform()}>정보수정</div>
         </div>
         <div className='tabTitle'>
             <div className='tabtext'>회원탈퇴</div>
