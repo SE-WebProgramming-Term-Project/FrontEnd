f37a9605e8131d2416171d9b061022acbceb6224
ikellllllll <shkm1420@naver.com>
2023-06-09 08:38:02 +0900

    [Review :: post 요청 DB 연동 코드 추가]

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,50 +1,85 @@
+import { useState } from "react";
+import "./css/Review.css";
+import axios from "axios";
+const Review = (props) => {
+  const [number, setNum] = useState();
+  const [txt, setTxt] = useState();
 
-import { useState } from 'react';
-import './css/Review.css';
-import axios from 'axios';
-const Review = (props)=> {
-  const [number, setNum] = useState()
-  const [txt, setTxt] = useState()
+  const handlenum = (e) => {
+    setNum(e.target.value);
+  };
+  const handletxt = (e) => {
+    setTxt(e.target.value);
+  };
+  // const sendReview = () => {
+  //   let data = {
+  //     ReviewNum: 1,
+  //     score: number,
+  //     reviewContent: txt,
+  //     writer: props.id,
+  //     product: props.orderMenu,
+  //   };
+  //   axios
+  //     .post("/json/user.json", data)
+  //     .then((response) => {
+  //       console.log("Data sent successfully!", response);
+  //     })
+  //     .catch((error) => {});
+  // };
 
-  const handlenum = (e) =>{
-    setNum(e.target.value)
-  }
-  const handletxt = (e) =>{
-    setTxt(e.target.value)
-  }
+  // const sendReview = () => {
+  //   console.log(props);
+  //   const reviews = props.orderMenu.map((id) => {
+  //     return {
+  //       id: 4,
+  //       score: number,
+  //       evaluation: txt,
+  //       author: props.id,
+  //       pizzaId: id,
+  //     };
+  //   });
+  //   axios
+  //     .post("http://localhost:5000/review/submit", reviews)
+  //     .then((response) => {
+  //       console.log("Data sent successfully!", response);
+  //     })
+  //     .catch((error) => {});
+  // };
+
   const sendReview = () => {
-    let data = {
-      ReviewNum : 1,
-      score : number,
-      reviewContent : txt,
-      writer : props.id,
-      product : props.orderMenu
-    }
-    axios.post("/json/user.json" , data)
-    .then(response => {
-        console.log('Data sent successfully!', response);
-        
+    console.log(props.data.orderMenu);
+  
+    const reviews = props.data.orderMenu.map((id) => ({
+      id: 4,
+      score: number,
+      evaluation: txt,
+      author: props.data.id,
+      pizzaId: id,
+    }));
+  
+    axios.post("http://localhost:5000/review/submit", reviews)
+      .then((response) => {
+        console.log("Data sent successfully!", response);
       })
-      .catch(error => {
-        
+      .catch((error) => {
+        // 에러 처리 로직 추가
+        console.log(error);
       });
-}
-
+  };
 
   return (
     <div className="reviewcontainer">
-        <div className='starcon'>
-      <input className='star' type='number' onChange={handlenum}></input>
+      <div className="starcon">
+        <input className="star" type="number" onChange={handlenum}></input>
       </div>
-      <div className='textcon'>
-      <textarea className='reviewText' onChange={handletxt}></textarea>
+      <div className="textcon">
+        <textarea className="reviewText" onChange={handletxt}></textarea>
       </div>
-      <div className='btncon'>
-      <input className='submitbtn' type='submit' onClick={sendReview}></input>
-      </div> 
+      <div className="btncon">
+        <input className="submitbtn" type="submit" onClick={sendReview}></input>
+      </div>
     </div>
   );
-}
-
+};
 
 export default Review;
