1595365186ca533835b209d5cb2d73e3e3e88288
SIron17 <jsca10021@gmail.com>
2023-06-11 15:05:28 +0900

    [Front : 메뉴별 판매수량, 요일별 총 매출 완료]

--- src/components/AdminMypage/AdminPageAnalysis.js
+++ src/components/AdminMypage/AdminPageAnalysis.js
@@ -1,80 +1,84 @@
-import { AdminPageSalesChart } from "./AdminPageSalesChart";
-import { AdminPageAmountChart } from "./AdminPageAmountChart";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import "./css/AdminPageAnalysis.css";
+import {AdminPageAmountChart} from "./AdminPageAmountChart";
+import {AdminPageSalesChart} from "./AdminPageSalesChart";
 
 function AdminPageAnalysis() {
-  const [salesByDayOfWeek, setSalesByDayOfWeek] = useState([
-    0, 0, 0, 0, 0, 0, 0,
-  ]);
-  const [pizzaSales, setPizzaSales] = useState({});
-  const [pizzalabels, setPizzalabels] = useState([]);
+
+  const [pizzaData, setPizzaData] = useState([]);
+  const [dayOfWeekLabels, setDayOfWeekLabels] = useState([]);
+  const [salesAmounts, setSalesAmounts] = useState([]);
 
   useEffect(() => {
-    axios
-      .get("http://localhost:5000/order/findAll")
-      .then((response) => {
-        const orders = response.data.orderData;
-        const salesByDayOfWeek = [0, 0, 0, 0, 0, 0, 0];
-        orders.forEach((order) => {
-          const orderDate = new Date(order.orderDate);
-          const dayOfWeek = orderDate.getDay();
-          salesByDayOfWeek[dayOfWeek] += order.totalPrice;
-        });
-        setSalesByDayOfWeek(salesByDayOfWeek);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+    axios.get('http://localhost:5000/order/findAll')
+        .then((response) => {
+          const orders = response.data.orderData;
+          const salesByDay = {};
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:5000/order/findAll")
-      .then((response) => {
-        const orders = response.data.orderData;
-        const pizzaSales = {};
-        orders.forEach((order) => {
-          order.orderMenu.forEach((pizzaId) => {
-            if (!pizzaSales[pizzaId]) {
-              pizzaSales[pizzaId] = 0;
-            }
-            pizzaSales[pizzaId]++;
+          const weekdays = ['일', '월', '화', '수', '목', '금', '토'];
+          weekdays.forEach((weekday) => {
+            salesByDay[weekday] = 0;
           });
+
+          orders.forEach((order) => {
+            const { orderDate, totalPrice } = order;
+            const weekday = getWeekdayFromOrderDate(orderDate);
+
+            salesByDay[weekday] += totalPrice;
+          });
+
+          const labels = Object.keys(salesByDay);
+          const amounts = Object.values(salesByDay);
+
+          setDayOfWeekLabels(labels);
+          setSalesAmounts(amounts);
+        })
+        .catch((error) => {
+          console.log(error);
         });
-        setPizzaSales(pizzaSales);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
   }, []);
 
+  const getWeekdayFromOrderDate = (orderDate) => {
+    const weekdays = ['일', '월', '화', '수', '목', '금', '토'];
+    const dateParts = orderDate.split('. ');
+    const timePart = dateParts[2];
+    const datePart = dateParts[1].replace(/\./g, '');
+    const dateString = `${datePart} ${timePart}`;
+    const orderDateObject = new Date(dateString);
+
+    let dayOfWeekIndex = orderDateObject.getDay();
+    if (dayOfWeekIndex === 0) {
+      dayOfWeekIndex = 7;
+    }
+
+    const weekday = weekdays[dayOfWeekIndex - 1];
+
+    return weekday;
+  };
+
   useEffect(() => {
-    const fetchPizzaNames = async () => {
-      try {
-        const pizzaNames = Object.keys(pizzaSales);
-        const responses = await Promise.all(
-          pizzaNames.map((_id) =>
-            axios.get("http://localhost:5000/pizza/view", { params: {_id} })
-          )
-        );
-        const fetchedPizzaLabels = responses.map(
-          (response) => response.data.pizzaData.title
-        );
-        setPizzalabels(fetchedPizzaLabels);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+    axios.get('http://localhost:5000/pizza/findAll')
+        .then((response) => {
+          const pizzas = response.data.pizzaData;
+          setPizzaData(pizzas);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+  }, []);
 
-    fetchPizzaNames();
-  }, [pizzaSales]);
+  const menuAmounts = {};
+  pizzaData.forEach((pizza) => {
+    const { title, count } = pizza;
+    if (!menuAmounts[title]) {
+      menuAmounts[title] = 0;
+    }
+    menuAmounts[title] += count;
+  });
 
-  const pizzaQuantities = Object.values(pizzaSales);
-
-  const labels2 = ["일", "월", "화", "수", "목", "금", "토"];
-  const sales = salesByDayOfWeek;
+  const labels = Object.keys(menuAmounts);
+  const amounts = Object.values(menuAmounts);
 
   return (
     <div className="admin-page-analysis">
@@ -82,13 +86,13 @@ function AdminPageAnalysis() {
         <div className="top-text">메뉴별 판매수량</div>
         <div className="chart-area">
           <AdminPageAmountChart
-            labels={pizzalabels}
-            amounts={pizzaQuantities}
+            labels={labels}
+            amounts={amounts}
           />
         </div>
         <div className="top-text">요일별 총 매출</div>
         <div className="chart-area">
-          <AdminPageSalesChart labels={labels2} sales={sales} />
+          <AdminPageSalesChart labels={dayOfWeekLabels.filter((label) => label !== undefined)} sales={salesAmounts} />
         </div>
       </div>
     </div>
