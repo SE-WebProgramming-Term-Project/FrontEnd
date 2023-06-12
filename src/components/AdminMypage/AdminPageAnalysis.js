64760e0b0239a4cfb419e9e506ec1bccab19e0a5
ikellllllll <shkm1420@naver.com>
2023-06-12 17:07:46 +0900

    [AdminPageAnalysis :: 차트 표시 수정]

--- src/components/AdminMypage/AdminPageAnalysis.js
+++ src/components/AdminMypage/AdminPageAnalysis.js
@@ -1,49 +1,67 @@
-import axios from "axios";
-import {useEffect, useState} from "react";
+import axios, { toFormData } from "axios";
+import { useEffect, useState } from "react";
 import "./css/AdminPageAnalysis.css";
-import {AdminPageAmountChart} from "./AdminPageAmountChart";
-import {AdminPageSalesChart} from "./AdminPageSalesChart";
+import { AdminPageAmountChart } from "./AdminPageAmountChart";
+import { AdminPageSalesChart } from "./AdminPageSalesChart";
 
 function AdminPageAnalysis() {
-
   const [pizzaData, setPizzaData] = useState([]);
   const [dayOfWeekLabels, setDayOfWeekLabels] = useState([]);
   const [salesAmounts, setSalesAmounts] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/order/findAll')
-        .then((response) => {
-          const orders = response.data.orderData;
-          const salesByDay = {};
+    axios
+      .get("http://localhost:5000/order/findAll")
+      .then((response) => {
+        const orders = response.data.orderData;
+        const salesByDay = {};
 
-          const weekdays = ['일', '월', '화', '수', '목', '금', '토'];
-          weekdays.forEach((weekday) => {
-            salesByDay[weekday] = 0;
-          });
+        const weekdays = ["월", "화", "수", "목", "금", "토", "일"];
+        weekdays.forEach((weekday) => {
+          salesByDay[weekday] = 0;
+        });
+
+        const today = new Date(); // 현재 날짜
+        const currentDayOfWeek = today.getDay(); // 현재 요일의 인덱스 (0: 일요일, 1: 월요일, ... , 6: 토요일)
+
+        console.log(today)
+
+        // 현재 날짜부터 이번 주의 월요일까지의 총 매출을 계산하여 salesByDay에 저장
+        for (let i = currentDayOfWeek - 1; i >= 0; i--) {
+          const targetDate = new Date(today);
+          targetDate.setDate(today.getDate() - i); // 현재 날짜에서 i일 이전의 날짜로 설정
+
+          const targetDateString = `${targetDate.getFullYear()}. ${
+            targetDate.getMonth() + 1
+          }. ${targetDate.getDate()}.`;
+          const weekday = getWeekdayFromOrderDate(targetDateString);
 
           orders.forEach((order) => {
             const { orderDate, totalPrice } = order;
-            const weekday = getWeekdayFromOrderDate(orderDate);
+            const orderWeekday = getWeekdayFromOrderDate(orderDate);
 
-            salesByDay[weekday] += totalPrice;
+            if (weekday === orderWeekday) {
+              salesByDay[weekday] += totalPrice;
+            }
           });
+        }
 
-          const labels = Object.keys(salesByDay);
-          const amounts = Object.values(salesByDay);
+        const labels = Object.keys(salesByDay);
+        const amounts = Object.values(salesByDay);
 
-          setDayOfWeekLabels(labels);
-          setSalesAmounts(amounts);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+        setDayOfWeekLabels(labels);
+        setSalesAmounts(amounts);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
 
   const getWeekdayFromOrderDate = (orderDate) => {
-    const weekdays = ['일', '월', '화', '수', '목', '금', '토'];
-    const dateParts = orderDate.split('. ');
+    const weekdays = ["일", "월", "화", "수", "목", "금", "토"];
+    const dateParts = orderDate.split(". ");
     const timePart = dateParts[2];
-    const datePart = dateParts[1].replace(/\./g, '');
+    const datePart = dateParts[1].replace(/\./g, "");
     const dateString = `${datePart} ${timePart}`;
     const orderDateObject = new Date(dateString);
 
@@ -58,14 +76,15 @@ function AdminPageAnalysis() {
   };
 
   useEffect(() => {
-    axios.get('http://localhost:5000/pizza/findAll')
-        .then((response) => {
-          const pizzas = response.data.pizzaData;
-          setPizzaData(pizzas);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+    axios
+      .get("http://localhost:5000/pizza/findAll")
+      .then((response) => {
+        const pizzas = response.data.pizzaData;
+        setPizzaData(pizzas);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
 
   const menuAmounts = {};
@@ -85,14 +104,14 @@ function AdminPageAnalysis() {
       <div className="admin-page-analysis-container">
         <div className="top-text">메뉴별 판매수량</div>
         <div className="chart-area">
-          <AdminPageAmountChart
-            labels={labels}
-            amounts={amounts}
-          />
+          <AdminPageAmountChart labels={labels} amounts={amounts} />
         </div>
         <div className="top-text">요일별 총 매출</div>
         <div className="chart-area">
-          <AdminPageSalesChart labels={dayOfWeekLabels.filter((label) => label !== undefined)} sales={salesAmounts} />
+          <AdminPageSalesChart
+            labels={dayOfWeekLabels.filter((label) => label !== undefined)}
+            sales={salesAmounts}
+          />
         </div>
       </div>
     </div>
